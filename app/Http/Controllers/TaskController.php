3bb74cbaa14379636a8a04280bb5f395159ecaf4
Daniel Briceno <daniel.b100726@gmail.com>
2024-06-20 21:48:30 -0400

    Make tasks migrations, model and controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+// php artisan make:controller TaskController --resource --model=Task ---> con este commando podemos crear un controlador y un modelo al mismo tiempo, añadiendo flags, con el flag --resource el controlador se crea con el el esqueleto basico de metodos para un CRUD
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
